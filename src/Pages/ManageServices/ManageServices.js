ddc2eaae8c96141c3739ecc39c757098a4307bdd
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-01-28 01:06:57 +0600

    delete single data

--- src/Pages/ManageServices/ManageServices.js
+++ src/Pages/ManageServices/ManageServices.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ManageServices = () => {
+    const[services,setServices]=useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/services')
+        .then(res=>res.json())
+        .then(data=>setServices(data))
+    },[]);
+
+    const handleDelete =id=>{
+        const url = `http://localhost:5000/services/${id}`;
+        fetch(url,{
+            method:"DELETE"
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            if(data.deletedCount){
+                alert('deleted')
+                const remaining = services.filter(service=>service._id!==id);
+                setServices(remaining);
+            
+            }
+        })
+    }
     return (
         <div>
             <h2>Manage Services</h2>
+            {
+                services.map(service=> <div key={service._id}>
+                    <h3>{service.name}</h3>
+                    <button onClick={()=>handleDelete(service._id)} className='btn btn-dark'>Delete</button>
+                </div> )
+            }
         </div>
     );
 };
