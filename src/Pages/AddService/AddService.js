7d84df509493756438416cb4df5044f23f799425
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-01-27 23:40:46 +0600

    connect backend

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -4,14 +4,15 @@ import { useForm } from "react-hook-form";
 import './AddService.css'
 
 const AddService = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit,reset } = useForm();
 
     const onSubmit = data => {
         console.log(data);  
         axios.post('http://localhost:5000/services',data) 
         .then(res=>{
             if(res.data.insertedId){
-                alert('Added Successfully')
+                alert('Added Successfully');
+                reset();
             }
             console.log(res);
         })
