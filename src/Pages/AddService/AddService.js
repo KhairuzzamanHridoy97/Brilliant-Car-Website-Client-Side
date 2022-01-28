ee83c68317626abc254244c01b631f09622424b4
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-01-28 19:19:44 +0600

    added heroku server

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -8,7 +8,7 @@ const AddService = () => {
 
     const onSubmit = data => {
         console.log(data);  
-        axios.post('http://localhost:5000/services',data) 
+        axios.post('https://floating-mesa-82570.herokuapp.com/services',data) 
         .then(res=>{
             if(res.data.insertedId){
                 alert('Added Successfully');
